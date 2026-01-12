f5b31ca5d6d4e128febc060d74692ff2edd77d27
Aryan Nair <aryanyouofficial3557u@gmail.com>
2026-01-12 07:54:41 +0530

    Binary Trees

--- Binary_Trees/BinaryTrees.java
+++ Binary_Trees/BinaryTrees.java
@@ -16,6 +16,7 @@
 import java.util.Queue;
 import java.util.LinkedList;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class BinaryTreesBasics{
 
@@ -354,16 +355,221 @@ public class BinaryTreesBasics{
             }
             System.out.println();
         }
-    }   
+        // ------------------------------------------------------------
+        // Q14. Print Nodes at Kth Level of Binary Tree
+        //
+        // About:
+        // Returns all nodes present at level K (root is level 1).
+        // Uses recursive traversal with a level counter.
+        //
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)  (recursion + list)
+        // ------------------------------------------------------------
+        public static ArrayList kthLevel(Node root,int k){
+            ArrayList<Integer> list=new ArrayList<>();
+            helper(root,k,1,list);
+            return list;
+        }
+
+        public static void helper(Node root,int k,int currIdx,ArrayList<Integer> list){
+            if(root == null){
+                return ;
+            }
+            if(currIdx == k){
+                list.add(root.data);
+            }
+            helper(root.left,k,currIdx+1,list);
+            helper(root.right,k,currIdx+1,list);
+        }
+
+        // ------------------------------------------------------------
+        // Q15. Find Path from Root to a Given Node
+        //
+        // About:
+        // Stores the path from root to a target node in an ArrayList.
+        //
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+        // ------------------------------------------------------------
+        public static boolean getPath(Node root,int n,ArrayList<Node> path){
+            if(root == null){
+                return false;
+            }
+            path.add(root);
+            if(root.data == n){
+                return true;
+            }
+            boolean isLeftFound=getPath(root.left,n,path);
+            boolean isRightFound=getPath(root.right,n,path);
+
+            if(isLeftFound || isRightFound){
+                return true;
+            }
+            path.remove(path.size()-1);
+            return false; 
+        }
+
+        // ------------------------------------------------------------
+        // Q16. Lowest Common Ancestor (Path Method)
+        //
+        // About:
+        // Finds LCA by storing paths from root to both nodes
+        // and comparing them.
+        //
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+        // ------------------------------------------------------------
+        public static Node lowestCommonAncestorApproach1(Node root,int n1,int n2){
+            ArrayList<Node> path1=new ArrayList<>();
+            ArrayList<Node> path2=new ArrayList<>();
+
+            getPath(root,n1,path1);
+            getPath(root,n2,path2);
+
+            int i=0,j=0;
+            Node lca=null;
+
+            while(i<path1.size() && j<path2.size()){
+                if(path1.get(i) == path2.get(j)){
+                    lca=path1.get(i);
+                }
+                i++;
+                j++;
+            }
+
+            return lca;
+        }
+
+        // ------------------------------------------------------------
+        // Q17. Lowest Common Ancestor (Optimized Recursive)
+        //
+        // About:
+        // Finds LCA without extra space using recursion.
+        //
+        // Time Complexity: O(n)
+        // Space Complexity: O(h)
+        // ------------------------------------------------------------
+        public static Node lowestCommonAncestorApproach2(Node root,int n1,int n2){
+            if(root == null || root.data ==n1 || root.data == n2){
+                return root;
+            }
+            Node leftLca=lowestCommonAncestorApproach2(root.left,n1,n2);
+            Node rightLca=lowestCommonAncestorApproach2(root.right,n1,n2);
+
+            if(leftLca == null){
+                return rightLca;
+            }
+            if(rightLca == null){
+                return leftLca;
+            }
+
+            return root;
+        }
+
+        // ------------------------------------------------------------
+        // Q18. Distance of a Node from Root
+        //
+        // About:
+        // Returns the number of edges between root and a target node.
+        //
+        // Time Complexity: O(n)
+        // ------------------------------------------------------------
+        public static int getDistance(Node root,int n){
+            if(root == null){
+                return -1;
+            }
+            if(root.data == n){
+                return 0;
+            }
+
+            int leftDistance=getDistance(root.left,n);
+            int rightDistance=getDistance(root.right,n);
+
+            if(leftDistance == -1 && rightDistance == -1){
+                return -1;
+            }else if(leftDistance == -1){
+                return rightDistance+1;
+            }else{
+                return leftDistance+1;
+            }
+        }
+
+        // ------------------------------------------------------------
+        // Q19. Minimum Distance Between Two Nodes
+        //
+        // About:
+        // Uses LCA + distance calculation.
+        //
+        // Time Complexity: O(n)
+        // ------------------------------------------------------------
+        public static int minDistance(Node root,int n1,int n2){
+            Node lca= lowestCommonAncestorApproach2(root,n1,n2);
+            int distance1=getDistance(lca,n1);
+            int distance2=getDistance(lca,n2);
+
+            return distance1+distance2;
+        }
+
+        // ------------------------------------------------------------
+        // Q20. Kth Ancestor of a Node
+        //
+        // About:
+        // Prints the Kth ancestor of a given node.
+        //
+        // Time Complexity: O(n)
+        // ------------------------------------------------------------
+        public static int kthAncestor(Node root,int n,int k){
+            if(root == null){
+                return -1;
+            }
+            if(root.data == n){
+                return 0;
+            }
+            int leftDistance=kthAncestor(root.left,n,k);
+            int rightDistance=kthAncestor(root.right,n,k);
+
+            int maxDistance=Math.max(leftDistance,rightDistance);
+            if(maxDistance != -1){
+                if(maxDistance+1 == k){
+                    System.out.println("root : "+ root.data);
+                }
+                return maxDistance+1;
+            }
+            return -1;
+        }
+
+        // ------------------------------------------------------------
+        // Q21. Convert to Sum Tree
+        //
+        // About:
+        // Replaces each node value with the sum of its left and right subtrees.
+        //
+        // Time Complexity: O(n)
+        // ------------------------------------------------------------
+        public static int sumtree(Node root){
+            if(root == null){
+                return 0;
+            }
+
+            int leftChildSum=sumtree(root.left);
+            int rightChildSum=sumtree(root.right);
+
+            int oldData=root.data;
+            root.data=leftChildSum+rightChildSum;
+
+            return oldData+root.data;
+        }
+
 
     // ------------------------------------------------------------
     // Main Method (Test Code)
     // ------------------------------------------------------------
     public static void main(String[] args){
-        int[] nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        int[] nodes={1,2,4,-1,-1,5,-1,-1,3,6,-1,-1,7,-1,-1};
         BinaryTree tree=new BinaryTree(); 
         Node root=tree.buildTree(nodes);
-
-        tree.topView(root);
+        // System.out.println(tree.kthAncestor(root,5,2));
+        tree.sumtree(root);
+        tree.preOrder(root);
     }
 }
